508a4d1cffc28e2966565bbb3d5c2791fd55988d
Travis Yatsko <travisyatsko@traviss-macbook-pro.local>
2018-07-17 16:06:55 -0700

    convert to MVC pattern

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  DataModel.swift
+//  Todoey
+//
+//  Created by Travis Yatsko on 7/17/18.
+//  Copyright © 2018 Travis Yatsko. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
