735dfdc0bc086e162d1ed1872462d24b74d8ffab
Travis Yatsko <travisyatsko@traviss-macbook-pro.local>
2018-07-17 17:16:48 -0700

    persisting data using codable

--- Todoey/Controller/TodoListViewController.swift
+++ Todoey/Controller/TodoListViewController.swift
@@ -12,18 +12,14 @@ class TodoListViewController: UITableViewController {
 
     //var itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
-        itemArray = items
-        }
+        loadItems()
         
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -53,17 +49,13 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done 
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        tableView.reloadData()
+        self.saveItems()
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark  {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
         
         tableView.deselectRow(at: indexPath, animated: true)
+        
     }
     //MARK - Add New Items
     
@@ -80,9 +72,7 @@ class TodoListViewController: UITableViewController {
             
             self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
-            
-            self.tableView.reloadData()
+            self.saveItems()
         }
         alert.addTextField { (alertTextField) in
             alertTextField.placeholder = "Create New Item"
@@ -94,7 +84,28 @@ class TodoListViewController: UITableViewController {
         
     }
     
+func saveItems() {
+    
+        
+        let encoder = PropertyListEncoder()
+    
+    do {
+    let data = try encoder.encode(itemArray)
+    try data.write(to: dataFilePath!)
+    } catch {
+    }
+    self.tableView.reloadData()
     
     
 }
-
+    func loadItems() {
+        let data = try? Data(contentsOf: dataFilePath!)
+        let decoder = PropertyListDecoder()
+        do {
+            itemArray = try decoder.decode([Item].self, from: data!)
+        } catch {
+            print(error)
+        }
+        
+    }
+}
